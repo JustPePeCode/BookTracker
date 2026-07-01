7f6a2899146dcf73f52ee28ca12a85bb2da590cf
JustPePeCode <peterpauldd@hotmail.com>
2026-07-01 17:02:06 +0200

    add ef core repository

--- BookTracker.Api/Program.cs
+++ BookTracker.Api/Program.cs
@@ -1,16 +1,28 @@
 using BookTracker.Api.Application;
 using BookTracker.Api.Application.CreateBook;
 using BookTracker.Api.Storage;
+using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddEndpointsApiExplorer();
 
-builder.Services.AddSingleton<IBookRepository, InMemoryBookRepository>();
+builder.Services.AddDbContext<AppDbContext>(options =>
+{
+    options.UseSqlite(builder.Configuration.GetConnectionString("BookTracker"));
+});
+
+builder.Services.AddScoped<IBookRepository, EfBookRepository>();
 
 builder.Services.AddScoped<BookService>();
 builder.Services.AddSwaggerGen();
 var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    using var scope = app.Services.CreateScope();
+    scope.ServiceProvider.GetRequiredService<AppDbContext>().Database.EnsureCreated();
+}
 
 app.MapGet("/books", async (BookService service) => Results.Ok(await service.GetAllBooks()));
 
